be760662fa9f8634d1b530b444f73bd365c8cb62
Jtea <jintea519@gmail.com>
2021-05-13 16:17:23 +0900

    json send change

--- DL_main.py
+++ DL_main.py
@@ -2,25 +2,26 @@ from flask import Flask, request, jsonify
 import json, base64
 from PIL import Image
 from io import BytesIO
+import Send_json
 
 app = Flask(__name__)
 
-@app.route("/data", methods=['GET','POST'])
-def Pill():
-  if request.method == 'POST':                                                # Get json data
-    params = request.get_json()                                               # receive json data
-    with open('pill_image.json', 'w') as pill_file:                           # write json data
-      json.dump(params, pill_file)
-    if(WriteImage(params)):                                                   # check write image 
-      print("image write")
-      return "Success"
-    else:
-      print("image write failed")
-      return "image write failed"
-  elif request.method == 'GET':                                               # Send json data
-    with open('pill_image.json', 'r') as pill_json:
-      jsonData = json.load(pill_json)
-    return jsonify(jsonData)
+@app.route("/data", methods=['POST'])
+def GetJson():
+  params = request.get_json()                                               # receive json data
+  with open('pill_image.json', 'w') as pill_file:                           # write json data
+    json.dump(params, pill_file)
+  if(WriteImage(params)):                                                   # check write image 
+    print("image write")
+    return "Success"
+  else:
+    print("image write failed")
+    return "image write failed"
+
+  if(Send_json.SendJson):
+    return "json Send"
+  else:
+    return "Send Failed"
 
 def WriteImage(imjson):                                                     # image write
   try:
@@ -29,6 +30,12 @@ def WriteImage(imjson):                                                     # im
     return True
   except:
     return False
+
+@app.route("/test", methods=['POST'])
+def SendTest():
+  respon = request.get_json()
+  print(respon)
+  return "check Test"
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port="5000")
--- Send_json.py
+++ Send_json.py
@@ -2,4 +2,11 @@ import requests, json
 
 #주소에 알약 검색 서버 주소 json에 json파일
 #POST 형식으로 JSON데이터 전송
-res = requests.post('주소', json={})
+def SendJson():
+  try:
+    with open('pill_image.json', 'r') as pill_json:
+      jsonData = json.load(pill_json)
+    res = requests.post('주소', data=jsonData)
+    return True
+  except:
+    return False

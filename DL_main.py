3ac90cc6e79a17201e1249932891b27bf3fa6e57
Jtea <jintea519@gmail.com>
2021-05-13 15:17:43 +0900

    code change

--- DL_main.py
+++ DL_main.py
@@ -5,17 +5,22 @@ from io import BytesIO
 
 app = Flask(__name__)
 
-@app.route("/data", methods=['POST'])
+@app.route("/data", methods=['GET','POST'])
 def GetJson():
-  params = request.get_json()                                               # receive json data
-  with open('pill_image.json', 'w') as pill_file:                           # write json data
-    json.dump(params, pill_file)
-  if(WriteImage(params)):                                                   # check write image 
-    print("image write")
-    return "Success"
-  else:
-    print("image write failed")
-    return "image write failed"
+  if request.method == 'POST':
+    params = request.get_json()                                               # receive json data
+    with open('pill_image.json', 'w') as pill_file:                           # write json data
+      json.dump(params, pill_file)
+    if(WriteImage(params)):                                                   # check write image 
+      print("image write")
+      return "Success"
+    else:
+      print("image write failed")
+      return "image write failed"
+  elif request.method == 'GET':
+    with open('pill_image.json', 'r') as pill_json:
+      jsonData = json.load(pill_json)
+    return jsonify(jsonData)
 
 def WriteImage(imjson):                                                     # image write
   try:
@@ -24,12 +29,6 @@ def WriteImage(imjson):                                                     # im
     return True
   except:
     return False
-  
-@app.route("/pilldata", methods=['GET'])
-def SendJson():
-  with open('pill_image.json', 'r') as pill_json:
-    jsonData = json.load(pill_json)
-    return jsonify(jsonData)
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port="5000")

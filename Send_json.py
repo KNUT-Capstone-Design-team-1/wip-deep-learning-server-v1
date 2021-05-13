f90585c7e0adbcceda3e57cffd0bdb177810dea3
Jtea <jintea519@gmail.com>
2021-05-13 16:43:19 +0900

    just code change

--- Send_json.py
+++ Send_json.py
@@ -8,7 +8,7 @@ def SendJson():
     try:
         with open('pill_image.json', 'r') as pill_json:
             jsonData = json.load(pill_json)
-            res = requests.post('http://52.79.235.20:5000/test', data=jsonData)
+            res = requests.post('http://52.79.235.20:5000/test', files=jsonData)
         return True
     except:
         return False

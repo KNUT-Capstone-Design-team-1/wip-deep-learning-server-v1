fca178ba9242b836e8f3888640da0377702464db
Jtea <jintea519@gmail.com>
2021-06-08 21:23:53 +0900

    test

--- makejson.py
+++ makejson.py
@@ -2,13 +2,13 @@ import json
 
 def make_json_file(pill_shape, pill_text, pill_line=False):
     drugData = {
-        "drug_name" : "ID",
-        "drug_type" : "정제",
-        "drug_shape" : "원형",
-        "drug_color" : "하양",
+        "drug_name" : "",
+        "drug_type" : "",
+        "drug_shape" : "",
+        "drug_color" : "",
         "drug_line" : "없음"
     }
-    drugData['drug_shape'] = pill_shape
+    #drugData['drug_shape'] = pill_shape
     drugData["drug_name"] = pill_text
     # drugData["drug_line"] = pill_line
     try:

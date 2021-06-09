c405615508859aa5aace5da7e3e2c4c5f7bee932
Jtea <jintea519@gmail.com>
2021-06-09 12:36:10 +0900

    code change

--- makejson.py
+++ makejson.py
@@ -8,7 +8,7 @@ def make_json_file(pill_shape, pill_text, pill_line=False):
         "drug_color" : "",
         "drug_line" : "없음"
     }
-    #drugData['drug_shape'] = pill_shape
+    drugData['drug_shape'] = pill_shape
     drugData["drug_name"] = pill_text
     # drugData["drug_line"] = pill_line
     try:
